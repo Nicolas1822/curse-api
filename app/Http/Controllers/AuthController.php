02421404bc4dbf2cdc6b9393cd75a0fe96e057a7
Nicolas Gonzalez <ng33446@gmail.com>
2022-12-06 19:55:59 -0500

    login-api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use stdClass;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+            ->json(['data' => $user, 'access_token' => $token, 'token_type' => 'bearer',]);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()
+                ->json(['message' => 'Unauhtorized'], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+            ->json([
+                'message_one' => 'Hi ' . $user->name,
+                'accessToken' => $token,
+                'token_type' => 'bearer',
+                'user' => $user,
+            ]);
+    }
+
+    public function logout(Request $request)
+    {
+        if (Auth::check()) {
+            $request->user()->tokens->each(function ($token, $key) {
+                $token->delete();
+            });
+        }
+
+        return ['message' => 'session logout'];
+    }
+}
