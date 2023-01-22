19590aa3b88a796b0e2eec9fee4afc51bda20764
Nicolas Gonzalez <ng33446@gmail.com>
2023-01-22 13:42:40 -0500

    se agrega la ruta recuperacion de contrasea

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,10 +22,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::get('passwordrecovery', [AuthController::class, 'passwordRecovery']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('show', [AuthController::class, 'show']);
-    Route::patch('edit', [AuthController::class, 'edit']);
+    Route::get('show', [UserController::class, 'show']);
+    Route::patch('edit', [UserController::class, 'updatePassword'])->name('updatePassword');
+    Route::put('update', [UserController::class, 'updateUser']);
     Route::delete('delete', [AuthController::class, 'destroy']);
 });
